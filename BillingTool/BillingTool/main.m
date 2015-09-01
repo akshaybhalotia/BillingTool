ad4bd13e97fbffe49ddb863d288e24480985d546
Your Name <you@example.com>
2015-09-01 09:51:31 +0530

    Adding project

---
+++ BillingTool/BillingTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BillingTool
+//
+//  Created by Sameer Agarwal on 01/09/15.
+//  Copyright (c) 2015 iOSRookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
